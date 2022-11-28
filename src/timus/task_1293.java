c3595c0ea881f310fbedbb7b0d095c58e148c914
Kirill <Ksludnov@yandex.ru>
2022-11-29 00:19:11 +0500

    Timus_1293

---
+++ src/timus/task_1293.java
@@ -0,0 +1,23 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task_1293 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int res = (n * (a*b) ) * 2;
+
+
+        out.println(res);
+        out.flush();
+    }
+}
